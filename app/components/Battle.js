d15650e29e7480bc86e03c2e2f04607988d691d5
Rebecca Rose <rebecca.j.rose@hotmail.com>
2020-05-11 16:33:08 +1000

    Created API to retrieve data about github users and assign a score based on their profile

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
+import Results from './Results'
 
 
 function Instructions () {
@@ -120,6 +121,7 @@ export default class Battle extends React.Component {
         this.state ={
             playerOne: null,
             playerTwo: null,
+            battle: false
         }
         
         this.handleSumbit = this.handleSumbit.bind(this)
@@ -135,7 +137,11 @@ export default class Battle extends React.Component {
         })
     }
     render() {
-        const { playerOne, playerTwo } = this.state
+        const { playerOne, playerTwo, battle } = this.state
+
+        if(battle === true) {
+            return <Results playerOne={playerOne} playerTwo={playerTwo} />
+        }
 
         return (
             <React.Fragment>
@@ -167,6 +173,15 @@ export default class Battle extends React.Component {
                               />  
                         }
                     </div>
+
+                    {playerOne && playerTwo && (
+                        <button
+                            className='btn dark-btn btn-space'
+                            onClick={() => this.setState({battle: true})}
+                            >
+                                Battle
+                        </button>
+                    )}
                 </div>                  
             </React.Fragment>
         )
